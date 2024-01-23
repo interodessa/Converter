82e2e3c608690fa548ec5e96f118dbd1728de28d
interodessa <alexkotuz@gmail.com>
2024-01-24 01:15:00 +0200

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,17 @@
+package app;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter distance");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("1) km -> ml" + "/n" + "2) ml -> km");
+        Scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
+
+
+
+    }
+}
