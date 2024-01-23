f0bfd491f3df2ecb857bb8eaf399a3c3bfcf8306
interodessa <alexkotuz@gmail.com>
2024-01-24 01:22:51 +0200

    add enter data

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,13 +5,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Enter distance");
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
+        Scanner number = new Scanner(System.in);
+        int a = number.nextInt();
         System.out.println("1) km -> ml" + "/n" + "2) ml -> km");
-        Scanner sc = new Scanner(System.in);
-        int b = sc.nextInt();
+        Scanner choice = new Scanner(System.in);
+        int b = choice.nextInt();
+        if (b == 1){
 
+        }
+        if (b == 2){
 
+        }
+        else
+            System.out.println("error");
 
     }
 }
