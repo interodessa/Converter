66c329ea495e93f5298585c129ff27e60199e06e
interodessa <alexkotuz@gmail.com>
2024-01-24 01:35:25 +0200

    add function mlInKm

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,18 +6,23 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Enter distance");
         Scanner number = new Scanner(System.in);
-        int a = number.nextInt();
-        System.out.println("1) km -> ml" + "/n" + "2) ml -> km");
+        double a = number.nextInt();
+        System.out.println("1) ml -> km" + "/n" + "2) km -> ml");
         Scanner choice = new Scanner(System.in);
         int b = choice.nextInt();
         if (b == 1){
-
+            Main obj = new Main();
+            obj.mlInKm(a);
         }
         if (b == 2){
 
         }
-        else
+        if (b != 1 && b != 2)
             System.out.println("error");
 
+    }
+    public void mlInKm(double a){
+        double result = a * 1.61;
+        System.out.println("distance in km = " + result);
     }
 }
